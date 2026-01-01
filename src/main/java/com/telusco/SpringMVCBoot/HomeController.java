7250e74adb94b802576b695bfda64257d8073d4f
krishna <krishnakn140@gmail.com>
2026-01-01 17:40:30 +0530

    Added Model and ModelMap in Home Controller

--- src/main/java/com/telusco/SpringMVCBoot/HomeController.java
+++ src/main/java/com/telusco/SpringMVCBoot/HomeController.java
@@ -3,6 +3,8 @@ package com.telusco.SpringMVCBoot;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
@@ -16,14 +18,11 @@ public class HomeController {
     }
 
     @RequestMapping("add")
-    public ModelAndView add(@RequestParam("num1") int i, @RequestParam("num2") int j) {
-
-        ModelAndView mv = new ModelAndView();
-        mv.setViewName("result");
+    public String add(@RequestParam("num1") int i, @RequestParam("num2") int j, Model m) {
 
         int num3 = i + j;
-        mv.addObject("num3", num3);
+        m.addAttribute("num3", num3);
 
-        return mv;
+        return "result";
     }
 }
